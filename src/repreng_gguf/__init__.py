a5a16f38820978b307c3f213017d42b1f5d7fcb7
Matt Anderson <415442+mandersogit@users.noreply.github.com>
2026-07-22 15:17:04 -0400

    Export GGUF streaming interfaces

--- src/repreng_gguf/__init__.py
+++ src/repreng_gguf/__init__.py
@@ -1,5 +1,6 @@
 """GGUF-native representation-engineering primitives."""
 
+from .gguf_reader import GGUFReader, GGUFTensorInfo
 from .projection import (
     LoRAFactors,
     apply_lora,
@@ -8,12 +9,21 @@ from .projection import (
     left_contract_chunked,
     orthonormalize_directions,
 )
+from .quantization import GGMLQuantizationType, dequantize_rows, quantize_rows
+from .tensor_source import GGUFWeightSource, TensorChunk
 
 __all__ = [
+    "GGMLQuantizationType",
+    "GGUFReader",
+    "GGUFTensorInfo",
+    "GGUFWeightSource",
     "LoRAFactors",
+    "TensorChunk",
     "apply_lora",
     "compile_projection_factors",
+    "dequantize_rows",
     "explicit_project_weight",
     "left_contract_chunked",
     "orthonormalize_directions",
+    "quantize_rows",
 ]
